a41857958e6f819040925eab1a3e7c964dd02849
Siddhesh Sonawane <siddhesh3142w@gmail.com>
2026-08-16 11:42:30 +0530

    Make Relay aurora more yellow

--- Fennrise-Vercel-Ready/app/components/VoiceVisual.tsx
+++ Fennrise-Vercel-Ready/app/components/VoiceVisual.tsx
@@ -10,16 +10,16 @@ export default function RelayVisual() {
         <SoftAurora
           speed={0.6}
           scale={1.5}
-          brightness={0.9}
-          color1="#FFD84A"
-          color2="#F4B400"
+          brightness={0.88}
+          color1="#FFE34D"
+          color2="#FFD000"
           noiseFrequency={2.5}
           noiseAmplitude={1.0}
           bandHeight={0.5}
           bandSpread={0.9}
           octaveDecay={0.1}
           layerOffset={0.12}
-          colorSpeed={0.38}
+          colorSpeed={0.32}
           enableMouseInteraction={true}
           mouseInfluence={0.08}
         />
