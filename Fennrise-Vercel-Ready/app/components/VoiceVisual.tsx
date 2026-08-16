ee792edf0298f8ad81667494e6c676bcbbbf014b
Siddhesh Sonawane <siddhesh3142w@gmail.com>
2026-08-16 11:35:09 +0530

    Blend Relay SoftAurora into Fennrise theme

--- Fennrise-Vercel-Ready/app/components/VoiceVisual.tsx
+++ Fennrise-Vercel-Ready/app/components/VoiceVisual.tsx
@@ -10,18 +10,18 @@ export default function RelayVisual() {
         <SoftAurora
           speed={0.6}
           scale={1.5}
-          brightness={1.0}
-          color1="#f7f7f7"
-          color2="#e100ff"
+          brightness={0.78}
+          color1="#ffe082"
+          color2="#f4b400"
           noiseFrequency={2.5}
           noiseAmplitude={1.0}
           bandHeight={0.5}
           bandSpread={1.0}
           octaveDecay={0.1}
-          layerOffset={0}
-          colorSpeed={1.0}
+          layerOffset={0.12}
+          colorSpeed={0.42}
           enableMouseInteraction={true}
-          mouseInfluence={0.25}
+          mouseInfluence={0.08}
         />
       </div>
     </div>
