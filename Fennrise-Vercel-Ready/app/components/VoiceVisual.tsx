fd785c7f0e16e475dde16f89da18e9b4c71e3609
Siddhesh Sonawane <siddhesh3142w@gmail.com>
2026-08-16 11:41:05 +0530

    Make Relay SoftAurora yellow and web-matched

--- Fennrise-Vercel-Ready/app/components/VoiceVisual.tsx
+++ Fennrise-Vercel-Ready/app/components/VoiceVisual.tsx
@@ -10,16 +10,16 @@ export default function RelayVisual() {
         <SoftAurora
           speed={0.6}
           scale={1.5}
-          brightness={0.78}
-          color1="#ffe082"
-          color2="#f4b400"
+          brightness={0.9}
+          color1="#FFD84A"
+          color2="#F4B400"
           noiseFrequency={2.5}
           noiseAmplitude={1.0}
           bandHeight={0.5}
-          bandSpread={1.0}
+          bandSpread={0.9}
           octaveDecay={0.1}
           layerOffset={0.12}
-          colorSpeed={0.42}
+          colorSpeed={0.38}
           enableMouseInteraction={true}
           mouseInfluence={0.08}
         />
